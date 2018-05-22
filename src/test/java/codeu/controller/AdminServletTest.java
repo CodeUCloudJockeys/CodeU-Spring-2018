8c4a65cedd5bff151076a7421ffb38a620bdbe06
rafer45 <ataque992@hotmail.com>
2018-05-21 22:14:33 -0500

    Set admin servlet user store for tests

--- src/test/java/codeu/controller/AdminServletTest.java
+++ src/test/java/codeu/controller/AdminServletTest.java
@@ -57,6 +57,7 @@ public class AdminServletTest {
         .thenReturn(mockRequestDispatcher);
 
     mockUserStore = Mockito.mock(UserStore.class);
+    adminServlet.setUserStore(mockUserStore);
   }
 
   @Test
