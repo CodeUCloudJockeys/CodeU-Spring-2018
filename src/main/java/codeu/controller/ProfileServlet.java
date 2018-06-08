7d01de5e51743a4c1ee662b8446a6318a75b8e8e
eselenic <32230882+eselenic@users.noreply.github.com>
2018-06-08 13:21:33 -0500

    fixing errors

--- src/main/java/codeu/controller/ProfileServlet.java
+++ src/main/java/codeu/controller/ProfileServlet.java
@@ -11,9 +11,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** This servlet class is responsible the profile pages */
-public class ProfileServlet extends HttpServlet {
-
 /** This servlet class is responsible the profile pages*/
 public class ProfileServlet extends HttpServlet{
 
