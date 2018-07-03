dd7065c20fbdc7bfb53477225577e3739e3a64e8
jcolladokuri <jcollado@codeustudents.com>
2018-07-03 16:46:56 -0500

    Displaying list of activities

--- src/main/java/codeu/controller/ActivityFeedServlet.java
+++ src/main/java/codeu/controller/ActivityFeedServlet.java
@@ -17,6 +17,12 @@ package codeu.controller;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import codeu.model.data.User;
+import codeu.model.data.Conversation;
+import codeu.model.data.Message;
+import codeu.model.store.basic.ConversationStore;
+import codeu.model.store.basic.MessageStore;
+import codeu.model.store.basic.UserStore;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -28,19 +34,53 @@ public class ActivityFeedServlet extends HttpServlet {
    * variable used to keep track of activity, I'm thinking to create more variables as we go to
    * register different types of activities
    */
-  private List<String> activity;
-  /** used to keep track of users friends */
-  private List<String> friends_usernames;
+
+  /** Store class that gives access to Users. */
+  private UserStore userStore;
+
+  /** Store class that gives access to Conversations. */
+  private ConversationStore conversationStore;
+
+  /** Store class that gives access to Messages. */
+  private MessageStore messageStore;
+
+  /** List of all activities */
+  private List<String> activities;
+
+
+
 
   @Override
   public void init() throws ServletException {
     super.init();
-
-    /** hardcoded activity for now */
-    activity = Arrays.asList("Ricardo Joined", "Elona says hi", "Drew just left the conversation");
-    friends_usernames = Arrays.asList("Ricardo", "Elona", "Drew");
+    setUserStore(userStore.getInstance());
+    setConversationStore(conversationStore.getInstance());
+    setMessageStore(messageStore.getInstance());
   }
 
+  /**
+   * Sets the UserStore used by this servlet. This function provides a common setup method for use
+   * by the test framework or the servlet's init() function.
+   */
+  void setUserStore(UserStore userStore){
+    this.userStore = userStore;
+  }
+
+  /**
+   * Sets the MessageStore used by this servlet. This function provides a common setup method for
+   * use by the test framework or the servlet's init() function.
+   */
+  void setMessageStore(MessageStore messageStore){
+    this.messageStore = messageStore;
+  }
+
+  /**
+   * Sets the ConversationStore used by this servlet. This function provides a common setup method
+   * for use by the test framework or the servlet's init() function.
+   */
+  void setConversationStore(ConversationStore conversationStore){
+    this.conversationStore = conversationStore;
+  }
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
@@ -52,5 +92,19 @@ public class ActivityFeedServlet extends HttpServlet {
     } else {
       request.getRequestDispatcher("/WEB-INF/view/activityfeed.jsp").forward(request, response);
     }
+    List<Conversation> conversations = conversationStore.getConversationList();
+    List<User> users = userStore.getUserList();
+
+    for(Conversation conversation : conversations){
+      activities.add(conversation.getTitle());
+    }
+
+    for(User user : users){
+      activities.add(user.getName());
+    }
+
+    request.setAttribute("activities", activities);
+    request.getRequestDispatcher("WEB-INF/view/activityfeed.jsp");
+
   }
 }
